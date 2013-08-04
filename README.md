e6aa767c4f2acadbd2305e6ce64ab655f65b6397
Mike Bostock <mbostock@gmail.com>
2013-08-04 10:23:56 -0700

    Disable Ken Burns slideshow, too.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A little Chrome extension to hide that ghastly Yahoo! eyebrow on Flickr.
+
+Also disables that horrible Ken Burns slideshow when you accidentally click on a photo, because it’s horrible.
--- chrome/hide-flickr-eyebrow.chrome/main.js
+++ chrome/hide-flickr-eyebrow.chrome/main.js
@@ -1,2 +1,12 @@
 document.body.classList.remove("with-eyebrow");
-document.getElementById("eyebrow").style.display = "none";
+
+var e = document.querySelector("#eyebrow");
+if (e) e.style.display = "none";
+
+document.addEventListener("DOMSubtreeModified", function() {
+  var e = document.querySelector(".img-wrap a");
+  if (e) e.addEventListener("click", function(e) { e.stopPropagation(); }, true);
+
+  var e = document.querySelector(".bottom-controls");
+  if (e) e.style.display = "none";
+});
